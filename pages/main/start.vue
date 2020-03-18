1750ffa2e50f48d5d89b1f6aa3f7b7e06708ba22
1575994958@qq.com <a123456789..coding>
2020-03-18 12:13:00 +0800

    init

---
+++ pages/main/start.vue
@@ -0,0 +1,16 @@
+<template>
+	<view>
+		<button @click="startLogin()"> 登 录</button>
+	</view>
+</template>
+<script>
+	export default {
+		methods: {
+		    startLogin(){
+				uni.reLaunch({
+					url: '../main/test',
+				});
+		    }
+		}
+	}
+</script>
---
+++ pages/main/test.vue
@@ -0,0 +1,27 @@
+<template>
+	<view>
+		H5. Test showToast
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			}
+		},
+		onLoad: function () {
+			uni.showToast({
+				title: '账户登录信息已失效,请重新登录',
+				icon: 'none',
+				duration: 2000,
+				success: function(){  
+					uni.reLaunch({  
+						url: '../main/start',
+					});  
+				}
+			});
+			return;
+		}
+	}
+</script>
